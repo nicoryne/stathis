8183d415bfeb49d15497c2bbd77f3abf8887b522
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-15 19:11:04 +0800

    app and entry point setup

---
+++ mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/ActivityEntryPoint.kt
@@ -0,0 +1,18 @@
+package citu.edu.stathis.mobile
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ActivityEntryPoint : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {  }
+    }
+}
---
+++ mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/StathisApplication.kt
@@ -0,0 +1,9 @@
+package citu.edu.stathis.mobile
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StathisApplication : Application() {
+
+}
