7e40d3f602bf4fb1aa3b9f02d385b3ed048aa68e
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-15 19:05:46 +0800

    chore: added base gradle implementations

--- mobile/stathis-mobile/app/build.gradle.kts
+++ mobile/stathis-mobile/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -26,6 +30,11 @@ android {
             )
         }
     }
+    buildFeatures {
+        compose = true
+        dataBinding = false
+        viewBinding = false
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,11 +45,51 @@ android {
 }
 
 dependencies {
-
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Dagger Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Hilt + ViewModel
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Material 3
+    implementation(libs.androidx.material3)
+
+    // Android UI
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Material Icons
+    implementation(libs.androidx.material.icons.extended.android)
+
+    // Google Font
+    implementation(libs.androidx.ui.text.google.fonts)
+
+    // Data Store
+    implementation(libs.androidx.datastore.preferences)
+
+    // Jetpack Compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
 }
+
+kapt {
+    correctErrorTypes = true
+}
+
+
+
--- mobile/stathis-mobile/build.gradle.kts
+++ mobile/stathis-mobile/build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
+    id("com.google.dagger.hilt.android") version "2.56.1" apply false
 }
