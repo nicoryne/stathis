eeec9533ba60c674941316690c19e99a0588117e
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-16 23:00:23 +0800

    feat: init managers

--- mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/core/data/PreferencesMananger.kt
+++ mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/core/data/PreferencesMananger.kt
@@ -1,2 +1,43 @@
 package citu.edu.stathis.mobile.core.data
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "stathis_preferences")
+
+@Singleton
+class PreferencesManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    private val dataStore = context.dataStore
+
+    suspend fun clear() {
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+    companion object {
+        private val ONBOARDING_COMPLETED = booleanPreferencesKey("onboarding_completed")
+
+    }
+
+    suspend fun setOnboardingCompleted(completed: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[ONBOARDING_COMPLETED] = completed
+        }
+    }
+
+    val onboardingCompletedFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+        preferences[ONBOARDING_COMPLETED] != false
+    }
+}
--- mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/core/di/AppModule.kt
+++ mobile/stathis-mobile/app/src/main/java/citu/edu/stathis/mobile/core/di/AppModule.kt
@@ -1,4 +1,34 @@
 package citu.edu.stathis.mobile.core.di
 
+import android.content.Context
+import citu.edu.stathis.mobile.core.data.PreferencesManager
+import citu.edu.stathis.mobile.core.network.SupabaseInstance
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.SupabaseClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager {
+        return PreferencesManager(context)
+    }
+
+    @Provides
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabase(): SupabaseClient {
+        return SupabaseInstance.instance
+    }
 }
