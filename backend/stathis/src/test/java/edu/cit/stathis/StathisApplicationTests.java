57baa418fa0118c03a2c2398639e33182b3ed1d5
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-10-02 22:15:33 +0800

    [Feature] Add new model, posture classification, and tests

--- backend/stathis/src/test/java/edu/cit/stathis/StathisApplicationTests.java
+++ backend/stathis/src/test/java/edu/cit/stathis/StathisApplicationTests.java
@@ -3,6 +3,8 @@ package edu.cit.stathis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class StathisApplicationTests {
 
